3ef34192c0da9c3059cd75a738f1b69883947a61
rfbrady <rfbrady@email.wm.edu>
2018-10-20 13:40:55 -0400

    models and shell

--- root/blog/models.py
+++ root/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    def __str__(self):
+        return self.post_title
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    post_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class postContent(models.Model):
+
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content_text = models.TextField()
